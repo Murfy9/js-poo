a19c87cdea5748c440b8b4bf0d54c929c1062a25
Pedro Xavier <pedroxxavier@hotmail.com>
2023-09-04 15:34:22 -0300

    Adicionando comportamentos

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 class Cliente {
   nome;
   cpf;
+}
+
+class ContaCorrente {
   agencia;
-  saldo;
+  #saldo = 0;
+
+  sacar(valor) {
+    if (this.#saldo >= valor) {
+      this.#saldo -= valor;
+      return valor;
+    }
+  }
+
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+    this.#saldo += valor;
+  }
 }
 
 const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-console.log(cliente1);
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+const contaCorrenteRicardo = new ContaCorrente();
+const contaCorrenteAlice = new ContaCorrente();
+
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
