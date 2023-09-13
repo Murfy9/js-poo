e62db54100b18146c5062a7eb68aedf4de954ae5
Pedro Xavier <pedroxxavier@hotmail.com>
2023-09-13 14:15:24 -0300

    Polimorfismo

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./Conta/ContaCorrente.js";
 import { ContaSalario } from "./Conta/ContaSalario.js";
 import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaDeAutenticacao } from "./SistemaDeAutenticacao.js";
+
+// Contas
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
@@ -14,3 +19,12 @@ contaSalario.sacar(10);
 //console.log(contaCorrente);
 //console.log(contaPoupanca);
 //console.log(contaSalario);
+
+// Funcionarios
+
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha(123456789);
+const gerente = new Gerente("Ana", 5000, 1237895601);
+
+const estaLogado = SistemaDeAutenticacao.login(diretor, 123456789);
+console.log(estaLogado);
